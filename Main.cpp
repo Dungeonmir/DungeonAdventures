ebdaa4935799b6b5b35c0b1d632596ab841c1a08
dungeonmir <dungeonmir@gmail.com>
2021-02-14 20:32:29 +0300

    added actor class

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,5 @@
 ﻿#define NOMINMAX
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
 #include <libtcod.hpp>
 #include "soloud.h"
 #include "soloud_thread.h"
@@ -14,88 +12,8 @@ int hero_x = 75;
 int hero_y = 25;
 
 
-void hideCursor()
-{
-	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO info;
-	info.dwSize = 100;
-	info.bVisible = FALSE;
-	SetConsoleCursorInfo(consoleHandle, &info);
-
-
-
-
-	system("mode con cols=150 lines=50"); 
-	HANDLE  hout = GetStdHandle(STD_OUTPUT_HANDLE);
-
-
-
-
-	HANDLE hConsole = GetStdHandle(STD_INPUT_HANDLE);
-	DWORD prevConsoleMode = 0;
-	GetConsoleMode(hConsole, &prevConsoleMode);
-	SetConsoleMode(hConsole, prevConsoleMode & ~ENABLE_QUICK_EDIT_MODE);
-
-
-	
-}
-
-
-void goto_x_y(int x, int y)
-{
-	COORD c = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-
-void hero_move(char key)
-{
-	switch (key)
-	{
-	case 'w':
-	case 'W':
-		printf("\b");
-		printf(" ");
-		hero_y--;
-		goto_x_y(hero_x, hero_y);
-
-		break;
-
-	case 'a':
-	case 'A':
-		printf("\b");
-		printf(" ");
-		hero_x -= 2;
-		goto_x_y(hero_x, hero_y);
-
-		break;
-
-	case 's':
-	case 'S':
-		printf("\b");
-		printf(" ");
-		hero_y++;
-		goto_x_y(hero_x, hero_y);
-
-
-		break;
-	case 'd':
-	case 'D':
-		printf("\b");
-		printf(" ");
-		hero_x += 2;
-		goto_x_y(hero_x, hero_y);
-
-		break;
-
-	case 27: stop = true;
-
-		break;
-	}
-}
 void step() {
-	printf("\b");
-	printf("%c", hero_char);
+	
 }
 int main()
 {
@@ -109,7 +27,7 @@ int main()
 	soloud.play(gWave); // Play the wave
 	
 	int playerx = 40, playery = 25;
-	TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
+	TCODConsole::initRoot(100, 30, "libtcod", false);
 	while (!TCODConsole::isWindowClosed()) {
 		TCOD_key_t key;
 		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
@@ -124,7 +42,6 @@ int main()
 		TCODConsole::root->putChar(playerx, playery, '@');
 		TCODConsole::flush();
 	}
-	
 	soloud.deinit();
 	return 0;
 }
